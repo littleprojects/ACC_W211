33f4179980d6b8827e34e0296fc6bc3f03e497ae
littleprojects <david.lenk@live.de>
2025-10-02 13:57:35 +0200

    Add Radar information

---
+++ 03_PoC/radar_viewer.py
@@ -0,0 +1,91 @@
+"""
+Prof of Concept:
+
+Read out the radar information and display it
+"""
+
+
+"""
+This script log the CAN data and parse it with a CAN Database (DBC) to a MDF file
+
+
+"""
+
+import can
+import cantools
+from lib import utils
+from lib.Mdf import Mdf
+from lib.Logger import Logger
+
+
+# module name for LOGGING and CONFIG
+module_name = 'RADAR_LOG'
+# just the Version of this script, to display and log; update this at major changes
+module_version = '0.0.1'
+
+config = {
+    'bus_interface': 'VN1610',
+    #'bus_interface': 'vCAN',
+
+    'loglevel':                 'INFO',             # debug
+    #'loglevel':                 'DEBUG',             # debug
+    'can_0_dbc':                '../02_Sensor/ARS408_can_database_ch0.dbc',
+    'mdf_log_file':             'log/Radarlog_' + utils.date_time_str() + '.mf4',
+}
+
+log = Logger(module_name).logger
+log.setLevel(utils.parse_log_level(config['loglevel']))
+
+log.info('Init')
+
+db_0 = cantools.database.load_file(config['can_0_dbc'])
+
+mdf = Mdf(config['mdf_log_file'], log, db_0) #, save_interval=10000)
+
+# Erstelle eine Bus-Instanz
+bus = can.interface.Bus(channel='0', interface='vector', bitrate=500000, app_name=config['bus_interface'])
+
+i = 0
+
+log.info('Start Logging to: ' + config['mdf_log_file'])
+log.info(('Load DBC: ' + config['can_0_dbc']))
+
+# loop infinity
+try:
+    while True:
+
+        # wait for msgs for 1 sec - is a blocking function
+        msg = bus.recv(1)
+
+        if msg:
+            vehicle_msg_id = hex(msg.arbitration_id)
+
+            try:
+                decode_msg = db_0.decode_message(msg.arbitration_id, msg.data)
+
+                print(decode_msg)
+
+                #mdf.add_signals(decode_msg)
+
+                i += 1
+            except:
+                pass
+
+            if i == 1:
+                log.info('Recording started')
+
+            if i % 1000 == 0:
+                log.info('Msgs: ' + str(i))
+
+except KeyboardInterrupt:
+    log.info('Shut down bus')
+    # stop bus
+    bus.shutdown()
+
+    log.info(str(i) + ' Msgs logt ')
+
+    if i > 0:
+        mdf.write_mdf()
+
+    log.info('STOPPED Logging')
+
---
+++ 04_ACC/readme.md
@@ -0,0 +1,19 @@
+# ACC Controller
+
+## Sources
+
+### PID
+* https://itzwieseltal.wordpress.com/2020/06/02/python-pid-regler/
+* https://www.mathworks.com/help/simulink/slref/bumpless-control-transfer-between-manual-and-pid-control.html
+
+
+### Cruise Control & Limiter
+* https://ctms.engin.umich.edu/CTMS/index.php?example=CruiseControl&section=ControlPID
+* https://github.com/askmuhsin/Cruise-control-design-using-PID
+
+### ACC
+* https://ieeexplore.ieee.org/document/8337542 -> sign in required
+* https://ieeexplore.ieee.org/document/10427856 -> sign in required
+* https://www.mathworks.com/help/mpc/ug/adaptive-cruise-control-using-model-predictive-controller.html
+* https://www.mathworks.com/help/mpc/ref/adaptivecruisecontrolsystem.html (Block Docu)
+* https://www.tandfonline.com/doi/full/10.1080/03772063.2021.2012282?scroll=top&needAccess=true (56€)
